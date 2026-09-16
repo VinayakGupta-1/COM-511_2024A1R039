0d96ac4d8246bd1ae7e5d60417c03b10cb2a7e90
VinayakGupta-1 <vinayakgupta955@gmail.com>
2026-09-16 12:27:19 +0530

    Final Pattern

---
+++ Basics/70_Pattern_07.py
@@ -0,0 +1,10 @@
+# Write a python program to print a hallow square using stars.
+n = int(input("Enter the size: "))
+
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n - 1 or j == 0 or j == n - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
